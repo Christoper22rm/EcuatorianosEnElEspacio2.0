d551a8006573335d1ba903bbd4e9a6389935c82c
Christoper <98061653+Christoper22rm@users.noreply.github.com>
2022-06-18 20:42:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const translateX = document.querySelectorAll(".translateX");
+const translateY = document.querySelectorAll(".translateY");
+window.addEventListener("scroll" ,() => {
+    let scroll = window.pageYOffset;
+    translateX.forEach(element => {
+      let speed = element.dataset.speed;
+      element.style.transform = `translateX(${scroll * speed}px)`;
+    })
+})
